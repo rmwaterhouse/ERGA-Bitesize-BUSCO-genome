9ed27a30c2a3877d0d40abb28c693fff11c8f92a
Rob Waterhouse <robert.waterhouse@gmail.com>
2025-11-24 16:10:57 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,8 +4,8 @@ set -x
 # Fix permissions first
 sudo chown -R vscode:vscode /opt/conda
 
-# Now everything runs as vscode user
+# Now everything runs as vscode user with auto-yes flags
 conda install -c conda-forge mamba -y
-mamba env create -f environment.yml
-conda init bash
+mamba env create -f environment.yml -y
+conda init bash --no-user
 echo 'conda activate bitesize' >> ~/.bashrc
