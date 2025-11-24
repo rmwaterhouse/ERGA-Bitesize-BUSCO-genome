a86166d558ab38122a43b68ec135d32ad0cd9625
Rob Waterhouse <robert.waterhouse@gmail.com>
2025-11-24 11:29:09 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
+set -e
 
-# Add bioconda channels
+# Configure channels
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 
-# Install Miniconda and Mamba
-wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-# Initialize Mamba and update it
-conda update -n base -c defaults conda -y
+# Install mamba
 conda install -n base -c conda-forge mamba -y
 
-# Initialize the shell for mamba in the same terminal session
-mamba shell init --shell bash --root-prefix=~/.local/share/mamba
-source ~/.bashrc
+# Create environment
+mamba create -n bitesize python=3.10 bioconda::busco=5.8.0 -y
 
-# Create a new environment with Python 3 for compatibility with BUSCO
-mamba create -n bitesize bioconda::busco=6.0.0 -y
-
-# Activate the environment in the same terminal session
-source ~/.bashrc
-mamba activate bitesize
+# Auto-activate
+echo "conda activate bitesize" >> ~/.bashrc
