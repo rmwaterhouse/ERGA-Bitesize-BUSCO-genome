390019466514fdcba937ea37949430f8bdf16e2e
Rob Waterhouse <robert.waterhouse@gmail.com>
2025-11-24 16:36:51 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,5 +7,5 @@ sudo chown -R vscode:vscode /opt/conda
 # Now everything runs as vscode user with auto-yes flags
 conda install -c conda-forge mamba -y
 mamba env create -f environment.yml -y
-eval "$(mamba shell hook --shell bash)"
+echo 'eval "$(mamba shell hook --shell bash)"' >> ~/.bashrc
 echo 'mamba activate bitesize' >> ~/.bashrc
