06cb294f0595110513a0f7eb352be8a9765f91b9
Rob Waterhouse <robert.waterhouse@gmail.com>
2025-11-24 13:07:22 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -ex
 
+# Install mamba
 conda install -n base -c conda-forge mamba -y
+
+# Create environment from file
 mamba env create -f environment.yml
+
+# Properly initialize conda for the vscode user
+eval "$(conda shell.bash hook)"
 conda init bash
-echo "conda activate bitesize" >> ~/.bashrc
+
+# Add activation to bashrc AND bash_profile
+echo 'eval "$(conda shell.bash hook)"' >> ~/.bashrc
+echo 'conda activate bitesize' >> ~/.bashrc
